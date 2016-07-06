c89cf133582093209c1e1200d1f7ff320fbfa637
psinalberth <inalberth07@gmail.com>
2016-07-06 09:50:24 -0300

    Testando aplicação #1

--- resources/views/clientes/novo-cliente.blade.php
+++ resources/views/clientes/novo-cliente.blade.php
@@ -9,34 +9,34 @@
 		<div class="page-header">
 			<h2><i class="fa fa-user-plus pull-left"></i>Novo Cliente</h2>
 		</div>
-		
+
 		{!! Form::open(['method' => 'POST', 'action' => 'ClientesController@store']) !!}
 
 			<div class="form-group">
 				{!! Form::label('nome', 'Nome:') !!}
 				{!! Form::text('nome', null, ['class' => 'form-control']) !!}
 			</div>
-			
+
 			<div class="row">
 				<div class="col-sm-3">
 					<div class="form-group">
 						{!! Form::label('cep', 'CEP:') !!}
 						{!! Form::text('cep', null, ['class' => 'form-control', 'maxlength' => 8, 'onkeypress' => 'return event.charCode >= 48 && event.charCode <= 57']) !!}
-					</div>			
+					</div>
 				</div>
 
 				<div class="col-sm-7">
 					<div class="form-group">
 						{!! Form::label('logradouro', 'Logradouro:') !!}
 						{!! Form::text('logradouro', null, ['class' => 'form-control']) !!}
-					</div>					
+					</div>
 				</div>
 
 				<div class="col-sm-2">
 					<div class="form-group">
 						{!! Form::label('numero', 'Número:') !!}
 						{!! Form::text('numero', null, ['class' => 'form-control', 'onkeypress' => 'return event.charCode >= 48 && event.charCode <= 57']) !!}
-					</div>					
+					</div>
 				</div>
 			</div>
 
@@ -45,14 +45,14 @@
 					<div class="form-group">
 						{!! Form::label('complemento', 'Complemento:') !!}
 						{!! Form::text('complemento', null, ['class' => 'form-control']) !!}
-					</div>					
+					</div>
 				</div>
-				
+
 				<div class="col-sm-6">
 					<div class="form-group">
 						{!! Form::label('bairro', 'Bairro:') !!}
 						{!! Form::text('bairro', null, ['class' => 'form-control']) !!}
-					</div>					
+					</div>
 				</div>
 			</div>
 
@@ -61,26 +61,26 @@
 					<div class="form-group">
 						{!! Form::label('uf', 'UF:') !!}
 						{!! Form::text('uf', null, ['class' => 'form-control']) !!}
-					</div>					
+					</div>
 				</div>
-				
+
 				<div class="col-sm-6">
 					<div class="form-group">
 						{!! Form::label('cidade', 'Cidade:') !!}
 						{!! Form::text('cidade', null, ['class' => 'form-control']) !!}
-					</div>					
+					</div>
 				</div>
 
 				<div class="col-sm-4">
 					<div class="form-group">
 						{!! Form::label('telefone', 'Telefone:') !!}
 						{!! Form::text('telefone', null, ['class' => 'form-control', 'onkeypress' => 'return event.charCode >= 48 && event.charCode <= 57']) !!}
-					</div>					
+					</div>
 				</div>
 			</div>
 
 			<div class="modal-footer">
-		    	<input type="submit" class="btn btn-primary" label="Salvar"></input>
+		    	<input type="submit" class="btn btn-primary" value="Salvar"></input>
 		        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
 		    </div>
 
--- tests/TransportadoraTest.php
+++ tests/TransportadoraTest.php
@@ -1,26 +1,75 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+class TransportadoraTest extends TestCase {
 
-class TransportadoraTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testDeveAcessarPaginaInicial() {
-        
-        $this->visit('/transportadora')
-        	->see('Transportadora')
-        	->dontSee('Teste');
-    }
+	public function setUp() {
 
-    public function testDeveSelecionarClientes() {
-    	$this->visit('/transportadora')
-    		->click('Clientes')
-    		->seePageIs('/transportadora/clientes');
-    }
+		parent::setUp();
+	}
+
+	public function tearDown() {
+
+	}
+
+	public function setUpMockery() {
+
+		$mock = Mockery::mock(ClienteRepository::class );
+		App::instance(ClienteRepository::class , $mock);
+
+		return $mock;
+	}
+
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	public function testDeveAcessarPaginaInicial() {
+
+		$this->visit('/transportadora')
+		     ->see('Transportadora')
+		     ->dontSee('Teste');
+	}
+
+	/**
+	 * @depends testDeveAcessarPaginaInicial
+	 **/
+
+	public function testDeveSelecionarClientes() {
+
+		$this->visit('/transportadora')
+		     ->click('Clientes')
+		     ->seePageIs('/transportadora/clientes');
+	}
+
+	/**
+	 * @depends testDummy
+	 **/
+
+	public function testDeveCriarCliente() {
+
+		$this->visit('/transportadora/clientes/create')
+		     ->type('Paciente Zero', 'nome')
+		     ->type('0120700', 'cep')
+		     ->type('21', 'numero')
+		     ->type('1474884', 'telefone')
+		     ->press('Salvar')
+		     ->seePageIs('/transportadora/clientes');
+	}
+
+	public function testDeveRetornarQuantidadeClientes() {
+
+		$mock = $this->setUpMockery();
+
+		$mock->shouldReceive('count')->once()->andReturn(10);
+
+		$this->assertEquals(10, $mock->count());
+
+		Mockery::close();
+	}
+
+	public function testDummy() {
+
+		$this->assertTrue(false);
+	}
 }
